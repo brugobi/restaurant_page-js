d14e50164025eb3c029e37e34ea5ffd11d3cbdd0
brugobi <brgobbi@hotmail.com>
2020-11-04 16:15:56 -0500

    update the navbar using js and set up scrit serve and delete the script in the html

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,7 @@
 import 'bootstrap';
 import '../css/styles.scss';
+import navBar from './navBar';
+
+navBar();
+
+console.log('Hello');
--- src/js/navBar.js
+++ src/js/navBar.js
@@ -1,9 +1,13 @@
-const nav = () => {
+const navBar = () => {
   const startDiv = document.getElementById('content');
   const navContainer = document.createElement('nav');
   navContainer.classList.add('navbar', 'navbar', 'navbar-expand-lg', 'navbar-dark', 'bg-dark');
+  // navContainer.innerHTML = '<p>HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO</p>';
   const navButton = document.createElement('button');
   navButton.classList.add('navbar-toggler');
+  navContainer.appendChild(navButton);
+  //const myAtrr = { "type" : "button", "data-toggle" : "collapse", "data-target" : "#navbarNav", "aria-label" : "Toggle navigation", "aria-controls" : "navbarNav", "aria-expanded" : "false"  };
+  //Object.assign(navButton.setAttribute, myAtrr);
   navButton.setAttribute('type', 'button');
   navButton.setAttribute('data-toggle', 'collapse');
   navButton.setAttribute('data-target', '#navbarNav');
@@ -15,15 +19,22 @@ const nav = () => {
   navButton.appendChild(spanElem);
   const divList = document.createElement('div');
   divList.classList.add('collapse', 'navbar-collapse');
-  //const ul = document.createElement('ul');
-  // ul.classList.add('navbar-nav');
-  const node = document.createElement('li');
-  const homeNode = document.createTextNode('Home');
-  const menuNode = document.createTextNode('Menu');
-  const contactNode = document.createTextNode('Contact');
-  node.appendChild(homeNode);
-  node.appendChild(menuNode);
-  node.appendChild(contactNode);
-  document.getElementsByClassName('navbar-nav').appendChild(node).appendChild(divList);
+  divList.setAttribute('id', 'navbarNav');
+  const ul = document.createElement('ul');
+  ul.classList.add('navbar-nav');
+  const links = ['Home', 'Menu', 'Contact'];
+  links.forEach(link => {
+    const node = document.createElement('li');
+    node.classList.add('nav-item');
+    const anchor = document.createElement('a');
+    anchor.classList.add('nav-link');
+    anchor.textContent = link;
+    node.appendChild(anchor);
+    ul.appendChild(node);
+  });
+  divList.appendChild(ul);
+  navContainer.appendChild(divList);
   startDiv.appendChild(navContainer);
 };
+
+export default navBar;
