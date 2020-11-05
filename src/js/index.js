8110489929f4ec52034a5d00ff5174de52a2c157
brugobi <brgobbi@hotmail.com>
2020-11-05 13:42:33 -0500

    add home page using js

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,9 @@ import 'bootstrap';
 import '../css/styles.scss';
 import '../assets/barbecue.jpg';
 import navBar from './navBar';
+import home from './home';
 
 navBar();
+home();
 
 console.log('Hello');
