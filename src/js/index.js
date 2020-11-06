4e0a2776b8973fed801672a9cc49cc7a123dfbf8
brugobi <brgobbi@hotmail.com>
2020-11-05 21:15:48 -0500

    add contact page using js

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,10 @@ import '../css/styles.scss';
 import '../assets/barbecue.jpg';
 import navBar from './navBar';
 import home from './home';
+import contact from './contact';
 
 navBar();
 //home();
+contact();
 
 console.log('Hello');
