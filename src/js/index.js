a37f6da60afee44490c41e537048e08c518a8d71
brugobi <brgobbi@hotmail.com>
2020-11-09 15:45:31 -0500

    add one  bare-bone to the menu card in js

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,11 @@ import '../assets/barbecue.jpg';
 import navBar from './navBar';
 import home from './home';
 import contact from './contact';
+import menu from './menu';
 
 navBar();
 //home();
 //contact();
+menu();
 
 console.log('Hello');
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,5 +1,62 @@
 import meals from './meal';
 
-const MENU = () => {
-  const MEAL = [...meals];
+const menu = () => {
+  //const MEAL = [...meals];
+
+  const container = document.createElement('div');
+  container.classList.add('container-fluid');
+  const divContentMenu = document.createElement('div');
+  divContentMenu.classList.add('d-flex', 'justify-content-center', 'align-items-center', 'flex-wrap');
+  container.appendChild(divContentMenu);
+
+  //conatiner will reapet
+  const mealContainer = document.createElement('div');
+  mealContainer.classList.add('p-2', 'col-md-6', 'bd-highlight', 'text-center');
+  divContentMenu.appendChild(mealContainer);
+
+  const card = document.createElement('div');
+  card.classList.add('card', 'text-white', 'bg-dark', 'mb-3');
+  mealContainer.appendChild(card);
+  const row = document.createElement('div');
+  row.classList.add('row', 'no-gutters');
+  card.appendChild(row);
+
+  const divImg = document.createElement('div');
+  divImg.classList.add('col-md-4');
+  row.append(divImg);
+  const imgCard = document.createElement('img');
+  imgCard.classList.add('card-img');
+  //addimage
+  divImg.appendChild(imgCard);
+
+  const divBody = document.createElement('div');
+  divBody.classList.add('col-md-8');
+  row.appendChild(divBody);
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  divBody.appendChild(cardBody);
+  const cardTitle = document.createElement('h5');
+  cardTitle.classList.add('card-title');
+  cardTitle.innerHTML = 'title here';
+  cardBody.appendChild(cardTitle); //addtitle
+
+  const cardText = document.createElement('p');
+  cardText.classList.add('card-text');
+  cardText.innerHTML = "here is the Text";
+  cardBody.appendChild(cardText);
+
+  const cardPrice = document.createElement('p');
+  cardPrice.classList.add('card-text');
+  cardBody.appendChild(cardPrice);
+  const price = document.createElement('small');
+  price.classList.add('text-muted');
+  price.innerHTML = '$35'; //addprice
+  cardPrice.appendChild(price);
+
+  //top to here
+  const startDiv = document.getElementById('content');
+  startDiv.appendChild(container);
+  
 };
+
+export default menu;
